9059cc2008b355d1bc1a280b3dd254f2ef24cb01
“Xeostop1” <stop1@daum.net>
2025-08-04 19:07:13 +0900

    홈화면 메인다트 수정 1/2

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
 // lib/main.dart
 
+import 'package:counter_01/screens/home_screen.dart';
+import 'package:counter_01/screens/workout_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-
-<<<<<<< HEAD
-import 'screens/workout_screen.dart';
-import 'view_models/workout_viewmodel.dart';
-import 'view_models/tts_viewmodel.dart';
-=======
-import 'screens/home_screen.dart'; // 새로 만든 HomeScreen
-import 'screens/workout_screen.dart'; // 운동 화면
->>>>>>> 3f29e40 (홈화면 메인다트 수정 1/2)
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,17 +17,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => WorkoutViewModel()),
-        ChangeNotifierProvider(create: (_) => TtsViewModel()),
-      ],
-      child: MaterialApp(
-        title: 'Workout Counter',
-        home: const WorkoutScreen(),
-      ),
-=======
+
     return MaterialApp(
       title: 'Workout Counter',
       debugShowCheckedModeBanner: false, // 디버그 배너 제거
@@ -46,7 +29,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/workout': (context) => const WorkoutScreen(), // 네비게이션 이동용
       },
->>>>>>> 3f29e40 (홈화면 메인다트 수정 1/2)
+
     );
   }
 }
